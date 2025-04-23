000e11c21fe6453d99570e17cf54143d453f92de
YeonkyungKang <yeon97@gmail.com>
2025-04-23 12:16:18 +0900

    refact: Refactoring Code

--- fb.py
+++ fb.py
@@ -1,7 +1,12 @@
 for i in range(1, 15+1):
-	if i % 3 ==0:
-		print('fizz')
-	elif i % 5 == 0:
-		print('buzz')
+	fizz = (i % 3 == 0)
+	buzz = (i % 5 == 0)
+
+	if fizz and buzz:
+		print('FizzBuzz')
+	elif fizz:
+		print('Fizz')
+	elif buzz:
+		print('Buzz')
 	else:
 		print(f'{i}')
